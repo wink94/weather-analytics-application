6692c49fc7afb7185e50d891026ce09a2da3708f
windula_k <mwindula16@gmail.com>
2023-10-29 16:55:23 +0200

    data migration application

---
+++ weather-analytics-data-migration/main.py
@@ -0,0 +1,83 @@
+import json
+import pymysql
+import os
+from concurrent.futures import ThreadPoolExecutor
+import logging
+
+precip_set = set()
+
+def read_json_file(filename):
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+
+def process_data(item):
+    try:
+        item["Temperature (C)"] = round(item["Temperature (C)"], 1)
+        item["Apparent Temperature (C)"] = round(item["Apparent Temperature (C)"], 1)
+        item["Visibility (km)"] = round(item["Visibility (km)"], 1)
+        item["Formatted Date"] = str(item["Formatted Date"]).strip().split(".")[0]
+
+        return item
+    except Exception as exception:
+        logging.error(exception, exc_info=True)
+        raise exception
+
+
+def insert_to_db(item,connection):
+    try:
+
+        with connection.cursor() as cursor:
+            cursor.execute('''
+            INSERT INTO weather_analytics_data_db.weather_unnormalized
+        (observed_at, summary, precip_type, temperature_celsius, apprnt_temperature_celsius, humidity, wind_speed_km_h, wind_bearing_degrees, visibility_km, cloud_cover, pressure_millibar, daily_summary)
+            VALUES (%s, %s, %s, %s, %s,%s, %s,%s, %s, %s,%s, %s)
+            ''', (item["Formatted Date"], item["Summary"], item["Precip Type"], item["Temperature (C)"],
+                  item["Apparent Temperature (C)"], item["Humidity"], item["Wind Speed (km/h)"], item["Wind Bearing (degrees)"],
+                  item["Visibility (km)"], item["Loud Cover"], item["Pressure (millibars)"], item["Daily Summary"]))
+
+        connection.commit()
+    except Exception as exception:
+        logging.error(exception, exc_info=True)
+
+def process_json(json_data,connection):
+    # Process all data
+    for weather_data in json_data:
+        processed_data = process_data(weather_data)
+        print(processed_data)
+        insert_to_db(processed_data,connection)
+
+
+
+dbUrl='localhost'
+dbUser='root'
+dbPassword='root'
+database='weather_analytics_data_db'
+
+def create_db_connections():
+    return pymysql.connect(host=dbUrl, user=dbUser, password=dbPassword, db=database, autocommit=True)
+
+
+def execute():
+    connections = []
+    try:
+        # Get all JSON filenames in the current directory
+        json_files = [f for f in os.listdir() if f.endswith('.json')]
+
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            # Read all JSON files
+            connections = [create_db_connections() for _ in range(3)]
+            json_data = executor.map(read_json_file, json_files)
+            executor.map(process_json, json_data,connections)
+
+        print(precip_set)
+
+    except Exception as exception:
+        logging.error(exception, exc_info=True)
+    finally:
+        for conn in connections:
+            conn.close()
+
+
+if __name__ == '__main__':
+    execute()
---
+++ weather-analytics-data-migration/sql_queries.sql
@@ -0,0 +1,62 @@
+CREATE TABLE weather_analytics_data_db.weather_unnormalized (
+weather_id int NOT NULL AUTO_INCREMENT,
+observed_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+summary varchar(30) default null,
+precip_type ENUM ('rain', 'snow', 'sleet', 'hail') DEFAULT null,
+temperature_celsius decimal default 0.0,
+apprnt_temperature_celsius decimal default 0.0,
+humidity decimal default 0.0,
+wind_speed_km_h decimal default 0.0,
+wind_bearing_degrees int default 0,
+visibility_km decimal default 0.0,
+cloud_cover tinyint(1) default 0,
+pressure_millibar decimal default 0.0,
+daily_summary varchar(150) default null,
+primary key (weather_id)
+)
+
+CREATE TABLE weather_analytics_data_db.weather_data (
+weather_data_id int NOT NULL AUTO_INCREMENT,
+observed_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+summary varchar(30) default null,
+precip_type ENUM ('rain', 'snow', 'sleet', 'hail') DEFAULT null,
+temperature_celsius decimal default 0.0,
+apprnt_temperature_celsius decimal default 0.0,
+humidity decimal default 0.0,
+wind_speed_km_h decimal default 0.0,
+wind_bearing_degrees int default 0,
+visibility_km decimal default 0.0,
+cloud_cover tinyint(1) default 0,
+pressure_millibar decimal default 0.0,
+daily_summary varchar(150) default null,
+primary key (weather_data_id),
+unique key weather_id_observed_at (weather_data_id,observed_at)
+)
+
+INSERT INTO weather_analytics_data_db.weather_data
+(weather_data_id, observed_at, summary, precip_type, temperature_celsius, apprnt_temperature_celsius, humidity, wind_speed_km_h, wind_bearing_degrees, visibility_km, cloud_cover, pressure_millibar, daily_summary)
+select *
+from weather_unnormalized
+
+CREATE TABLE weather_analytics_data_db.weather_summary (
+weather_summary_id int NOT NULL AUTO_INCREMENT,
+summary varchar(30) default null,
+precip_type ENUM ('rain', 'snow', 'sleet', 'hail') DEFAULT null,
+daily_summary varchar(150) default null,
+primary key (weather_summary_id),
+unique key weather_summary_id_precip_type (summary,precip_type,daily_summary)
+)
+
+INSERT INTO weather_analytics_data_db.weather_summary
+(summary, precip_type, daily_summary)
+select distinct summary,  precip_type,  daily_summary
+from weather_unnormalized
+
+alter table weather_data add column weather_summary_id int NOT NULL
+
+UPDATE weather_analytics_data_db.weather_data wd
+join weather_analytics_data_db.weather_summary ws
+on wd.summary = ws.summary and wd.daily_summary = ws.daily_summary and wd.precip_type = ws.precip_type
+SET  wd.weather_summary_id = ws.weather_summary_id
+
+
